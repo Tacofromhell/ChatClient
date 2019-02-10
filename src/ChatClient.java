739eea2db64d1c4028ed1578e08fc7308e4df6f6
Martin <waldemarsson@gmail.com>
2019-02-10 01:17:07 +0100

    Updated with support for sending Objects

--- src/ChatClient.java
+++ src/ChatClient.java
@@ -3,27 +3,39 @@ import java.net.*;
 
 class ChatClient {
 
-    private final static ChatClient client = new ChatClient();
+//    private final static ChatClient client = new ChatClient();
     private final String HOSTNAME = "localhost";
     private final int PORT = 1234;
+    private boolean running = true;
 
-    private ChatClient() {
-        try (
+    public ChatClient() {
+        try {
             Socket socket = new Socket(HOSTNAME, PORT);
-            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in))
-
-        ) {
+            //TODO: add setSoTimeout()
+            ObjectOutputStream dataOut = new ObjectOutputStream(socket.getOutputStream());
+            ObjectInputStream dataIn = new ObjectInputStream(socket.getInputStream());
+            BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
 
             System.out.println("Connected");
-            String userInput;
-            while ((userInput = stdIn.readLine()) != null) {
-                out.println(userInput);
+            while (running) {
+            String userInput = input.readLine();
+//            dout.writeUTF(userInput);
+//            String msg = din.readUTF();
+            Message msg = new Message(socket, userInput);
+            dataOut.writeObject(msg);
+            dataOut.flush();
                 if (userInput.equals("quit")) {
                     socket.close();
                 }
-                System.out.println(socket.getLocalSocketAddress().toString().substring(1) + " " + in.readLine());
+
+                Message incoming = null;
+                try {
+                    incoming = (Message)dataIn.readObject();
+                } catch (ClassNotFoundException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(incoming.getTimestamp() + " | " + incoming.getSender().substring(1) + ": " + incoming.getMsg());
+//                System.out.println(socket.getLocalSocketAddress().toString().substring(1) + " " + msg);
             }
         } catch (UnknownHostException e) {
             System.err.println("Don't know about host " + HOSTNAME);
@@ -35,8 +47,8 @@ class ChatClient {
         }
     }
 
-    static ChatClient get(){
-        return client;
+    public ChatClient get(){
+        return this;
     }
 }
 
