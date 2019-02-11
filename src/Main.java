ff78a72d700c5e43ef200ee0409b4976f47dabf1
Aarkan <johanwiren1@gmail.com>
2019-02-11 11:18:07 +0100

    new branch

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
-public class Main {
-    public static void main(String[] args){
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+
         new ChatClient();
 
     }
